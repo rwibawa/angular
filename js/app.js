301008fa8c66fef9e47890c952d8d5fd9f4dd1c3
Ryan Wibawa <ryan.wibawa@gmail.com>
2016-08-16 22:23:04 -0700

    from single product to products.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,20 @@
 		this.products = gems;
 	});
 	
-	var gems = {
+	var gems = [
+		{
 			name: "Dodecahedron",
 			price: 2.95,
 			description: "Some gems have hidden qualities beyond their luster, beyond their shine.. Dodecahedron is one of them",
 			canPurchase: true,
-			soldOut: true
-	}
+			soldOut: false
+		},
+		{
+			name: "Pentagonal Gem",
+			price: 5.95,
+			description: "Some gems have hidden qualities beyond their luster, beyond their shine.. Pentagonal Gem is one of them",
+			canPurchase: true,
+			soldOut: false
+		}
+	];
 })();
