f41115618f978fbafc72555c66ed17bf531e174c
Ryan Wibawa <ryan.wibawa@gmail.com>
2016-08-27 23:05:51 -0700

    Get the data from ajax call.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 (function() {
 	var app = angular.module('store', ['store-directives']);
 	
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
+	app.controller('StoreController', [ '$http', function($http){
+		var store = this;
+		store.products = [];
+		$http.get('includes/store-products.json').success(function(data){
+			store.products = data;
+		});
+	} ]);
 	
 	app.controller('PanelController', function(){
 		this.tab = 1;
